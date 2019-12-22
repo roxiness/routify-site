2a0951dbd696b9a60725d261b6900a81397b6715
Jakob Rosenberg <jakobrosenberg@gmail.com>
2019-12-22 11:00:28 +0100

    temp fix redirect

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,6 +1,8 @@
 <script>
     import { goto } from '@sveltech/routify'
-    $goto('../docs/introduction')
+    setTimeout(() => {        
+        $goto('../docs/introduction')
+    });
 </script>
 
 <h1 class="text-xl bg-gray-100">Redirecting to introduction</h1>
