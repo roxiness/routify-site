f89f58ae194fe2c3127aec75c14c18b63f9272d5
Jakob Rosenberg <jakobrosenberg@gmail.com>
2019-12-28 13:38:32 +0100

    remove unneeded timeout

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,8 +1,8 @@
 <script>
     import { goto } from '@sveltech/routify'
-    setTimeout(() => {        
-        $goto('../docs/introduction')
-    });
+    
+        $goto('../docs/introduction', {})
+    
 </script>
 
 <h1 class="text-xl bg-gray-100">Redirecting to introduction</h1>
